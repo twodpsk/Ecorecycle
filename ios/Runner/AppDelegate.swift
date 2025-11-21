d1ff2b968301dc59f7d84e99c3f140a6c52d3570
이예나 <yena@iyenaui-MacBookAir.local>
2025-11-21 20:46:07 +0900

    Update AppDelegate.swift after removing API key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps  // Pod 설치 후 인식됨
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDdb3Iyom_7wpmkJkyRckaI6fzuEn-uh0A")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
